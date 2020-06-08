a32bb270cb67baa6b36ff455c7ba4169a9fe04db
Scott Bateman <scottb@unb.ca>
2020-06-08 13:56:42 -0300

    First commit of master branch

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 /**
  * Configure the server using the options below.
 */
-const HTTP_PORT = 9667;                         //server port
-const DB_NAME = "scary_game_condition.db";      //database name... this database file will be created unless already exists
-const CONDITIONS = ['play','watch'];            //provide the name of the conditions
-const CONDITION_SERVER_PATH = "condition";      //the directory path on the server where conditions will be served from... 
-                                                //can be "empty"
+const HTTP_PORT = 9667;                             //server port
+const DB_NAME = "default_condition.db";             //database name... this database file will be created unless already exists
+const CONDITIONS = ['condition1','condition2'];     //provide the name of the conditions
+const CONDITION_SERVER_PATH = "condition";          //the directory path on the server where conditions will be served from... 
+                                                    //can be "empty"
 //exports... do not touch
 module.exports.HTTP_PORT = HTTP_PORT;
 module.exports.DB_NAME = DB_NAME;
